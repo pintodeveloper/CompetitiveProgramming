d7d274d08fab96e8353adb58db2e89d264c49f3e
pintodeveloper <pintodeveloper@gmail.com>
2026-01-06 16:08:08 -0500

    feat: add new solution the exercise the 50A in c++

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,M;
+    cin>>N>>M;
+
+    float n = N*M;
+    cout<<floor(n/2)<<"\n";
+
+    return 0;
+}
