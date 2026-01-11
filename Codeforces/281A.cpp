85880b10658108f76bc43a1c2405fc00ea15d9dc
pintodeveloper <pintodeveloper@gmail.com>
2026-01-11 12:40:27 -0500

    feat: add new exercise the codeforces category A

---
+++ Codeforces/281A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string word;
+    cin>>word;
+
+    word[0] = toupper(word[0]);
+
+    cout<<word<<"\n";
+
+
+
+    return 0;
+}
