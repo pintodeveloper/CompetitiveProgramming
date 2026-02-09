a72bba5abc754e7fc64fe10794ebf371ed2dfc11
pintodeveloper <pintodeveloper@gmail.com>
2026-02-08 22:04:54 -0500

    feat: add new exercise the codeforces

---
+++ Codeforces/1352.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    parts = []
+    pos = 1
+
+
+    while(n>0):
+        dig = n% 10
+        if dig != 0:
+            parts.append(dig*pos)
+        n //=10
+        pos*=10
+    
+    print(len(parts))
+    print(*parts)
